9758606ce60daa1469d1e4701331e3413a09af81
Eyosiyas Ketema <keyosiyas@gmail.com>
2017-05-28 18:43:41 -0700

    Eyosiyas 2nd

--- Documents/NetBeansProjects/advanced/src/advanced/ThreadExcercise.java
+++ Documents/NetBeansProjects/advanced/src/advanced/ThreadExcercise.java
@@ -65,10 +65,49 @@ class Thread3 extends Thread{
     public int getSum3(){
         return sum3;
     }
-ystem.out.println("the sum of the thread 3 is= "+sum3);
+//ystem.out.println("the sum of the thread 3 is= "+sum3);
+//
+//    }
+//    public int getSum3(){
+//        return sum3;
+    }
+public class lastlab {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(helloworld.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(helloworld.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(helloworld.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(helloworld.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new helloworld().setVisible(true);
+                
+            }
+        });
     }
-    public int getSum3(){
-        return sum3;
-    }
+}
+
 }
